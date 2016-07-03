e3845e1b80373eec744d0e61a34aeff8c0401536
sabazusi <kouhei.live@gmail.com>
2016-07-03 09:17:55 +0900

    Add login props

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
         Ajisai Login
+        status: {this.props.isLoggedIn ? 'welcome' : 'login'}
       </div>
     );
   }
