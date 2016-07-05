fc53a5c45b37c5b1f097ee625509cfe9f53536eb
sabazusi <kouhei.live@gmail.com>
2016-07-06 01:18:52 +0900

    Add css file with css-modules

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.css?$/,
+        loaders: [
+        'style?sourceMap',
+        'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ],
+        include: path.resolve(__dirname, '../')
+      }
+    ]
+  }
+}
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './login.style.css';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -6,7 +7,7 @@ export default class Login extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className={styles.login}>
         Ajisai Login
         status: {this.props.isLoggedIn ? 'welcome' : 'login'}
       </div>
