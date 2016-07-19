fd5bdcd6a998810e6c2931bcbc9636732e12281c
sabazusi <kouhei.live@gmail.com>
2016-07-19 11:30:17 +0900

    Add starter class

--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@ import dotenv from 'dotenv';
 import {PATHS} from './constants/local-files';
 import {IPC} from './constants/ipc';
 import {createInitialWindow} from './utils/app-windows';
+import {start} from './main-starter';
 
 app.on('ready', () => {
   // load from .env
   dotenv.config();
-  
+
   // setup ipc handler
   ipcMain.on(IPC.login, (e, keys) => {
-    console.log(keys);
-    console.log('hello');
+    start(keys);
   });
 
-  console.log(process.env.CALLBACK_URL);
   const initialWindow = createInitialWindow();
   initialWindow.loadURL(PATHS.initialTemplate);
 });
