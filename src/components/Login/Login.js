9219bb9db160d844997c9603dda4ae1738c5b35a
sabazusi <kouhei.live@gmail.com>
2016-08-15 23:21:34 +0900

    Implement Logged in accounts status in Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,49 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  static propTypes = {
+    verifiedUsers         : React.PropTypes.array,
+    onClickConfirmAccounts: React.PropTypes.func
+  };
+
+  static defaultProps = {
+    verifiedUsers         : []
+  };
+
   constructor(props) {
     super(props);
   }
+
+  getStatusLabel() {
+    const verifiedUsers = this.props.verifiedUsers;
+    return verifiedUsers.length > 0 ?
+      verifiedUsers.map((user) => {
+        return (
+          <div>
+            <img src={user.profile_image_url} width={50} height={50} />
+            {user.screen_name}
+          </div>
+        );
+      }) : 'logining...';
+  }
+
+  getConfirmButton() {
+    return this.props.verifiedUsers.length > 0 ?
+      (
+        <button
+          onClick={::this.props.onClickConfirmAccounts}
+        >
+          OK
+        </button>
+      ) : null;
+  }
+
   render() {
     return (
       <div className="login">
         Ajisai Login
-        status: {this.props.isLoggedIn ? 'welcome' : 'login'}
+        status: {this.getStatusLabel()}
+        {this.getConfirmButton()}
       </div>
     );
   }
