ec1391369a6d462aef2121b6c7093f8b05bd4bdc
sabazusi <kouhei.live@gmail.com>
2016-08-18 01:18:29 +0900

    Add store creation class

--- src/main-window.js
+++ src/main-window.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {ipcRenderer} from 'electron';
-import {createStore} from 'redux';
 import Root from './containers/Root';
 import {IPC} from './constants/ipc';
 import TwitterClient from './clients/twitter-client';
 import {KEYS} from './constants/local-storage';
 import LocalStorage from './utils/local-storage';
+import {configureStore} from './store/store';
 
 window.onload = () => {
   loginToTwitter();
-  const store = createStore(()=>{}, []);
+
+  const store = configureStore();
   ReactDOM.render(
     <Provider store={store}>
       <Root />
